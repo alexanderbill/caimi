de7bf1c1713d8b264e77d9fa20692e45446e310e
leizhou <leizhou@oupeng.com>
2015-03-23 13:22:24 +0800

    fixup

--- CaimiTest/src/com/caimi/test/LoginTest.java
+++ CaimiTest/src/com/caimi/test/LoginTest.java
@@ -2,6 +2,11 @@ package com.caimi.test;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 import com.robotium.solo.Solo;
 
@@ -12,7 +17,7 @@ public class LoginTest extends ActivityInstrumentationTestCase2 {
 	public Activity activity;
 	private static Class<?> launchActivityClass;
 	// 对应re-sign.jar生成出来的信息框里的两个值
-	private static String mainActiviy = "caimi.qidong.activity.SplashActivity";
+	private static String mainActiviy = "caimi.qidong.activity.MainTabActivity";
 	private static String packageName = "com.caimi";
 
 	static {
@@ -32,16 +37,42 @@ public class LoginTest extends ActivityInstrumentationTestCase2 {
 	protected void setUp() throws Exception {
 		super.setUp();
 		this.activity = this.getActivity();
-		// this.solo = new Solo(getInstrumentation(), getActivity());
+		this.solo = new Solo(getInstrumentation(), getActivity());
+
+		TabWidget tab = (TabWidget) solo.getView("id/tabs");
+		View login = tab.getChildAt(4);
+		solo.clickOnView(login);
+
+		solo.sleep(5000);
+
+		EditText pass = (EditText) solo.getView("id/et_password");
+		solo.enterText(pass,"hfr111712");
+		Button btn = (Button) solo.getView("id/bt_login");
+		solo.clickOnView(btn);
+
+		solo.sleep(5000);
 	}
 
 	public void testLoginWithIncorrentUsernameAndPassword() throws Exception {
-		wait(5000);
-		// 待完成
+
 	}
 
 	@Override
 	public void tearDown() throws Exception {
+		TabWidget tab = (TabWidget) solo.getView("id/tabs");
+		View login = tab.getChildAt(4);
+		solo.clickOnView(login);
+
+		solo.sleep(5000);
+
+		TextView config = (TextView) solo.getView("id/tv_nickname");
+		solo.clickOnView(config);
+		solo.waitForActivity("caimi.gerenzhongxin.dengluzhuce.activity.NewMyCenterActivity");
+		View logout = solo.getView("id/btn_tcdl");
+		solo.clickOnView(logout);
+
+		solo.sleep(5000);
+
 		try {
 			this.solo.finishOpenedActivities();
 		} catch (Throwable e) {
